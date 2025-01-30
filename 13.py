5ee78e5fdc706d315fb73384ef622c9ad0ac09ef
Dagurechaff <box153m@ya.ru>
2025-01-30 21:52:01 +0300

    13.py

--- 13.py
+++ 13.py
@@ -1,7 +1,8 @@
-import itertools 
-count = 0
-s = itertools.product([0,1],repeat = 11)
-for i in s:
-    if (8 + sum(i))%5 !=0:
-        count += 1
-print(count)
+from ipaddress import *
+net = ip_network(f'172.16.168.0/255.255.248.0', 0)
+cnt = 0
+for ip in net:
+    s = f'{ip:b}'
+    if s.count('1') % 5 != 0:
+        cnt += 1
+print(cnt)
