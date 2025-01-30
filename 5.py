71f1c31a5c06bc0e15391a38eb369866aacffc35
Dagurechaff <box153m@ya.ru>
2025-01-30 20:48:59 +0300

    №5

---
+++ 5.py
@@ -0,0 +1,10 @@
+for N in range(100,10000):
+    m = []
+    s = str(N)
+    while len(s) >= 3:
+        m.append(int(s[:3]))
+        s = s[1:]
+    R = max(m) - min(m)
+    if R == 623:
+        print(N)
+        break
