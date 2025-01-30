f324ce3870866e5df23a6898b8d39fb4961191f7
Dagurechaff <box153m@ya.ru>
2025-01-30 23:04:07 +0300

    №24

---
+++ 24.py
@@ -0,0 +1,11 @@
+st = open('24 (1).txt', 'r').readline()
+k = 1
+mx = 0
+for i in range(1, len(st)):
+    if (st[i] == 'K' and st[i-1] == 'L') or (st[i-1] == 'K' and st[i] == 'L'):
+        k = 1
+    else:
+        k += 1
+        if k > mx:
+            mx = k
+print(mx)
